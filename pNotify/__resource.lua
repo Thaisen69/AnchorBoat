fdd6dadfb8ab8769378ebd291531cc843bcd6671
Thaisen69 <39800430+Thaisen69@users.noreply.github.com>
2018-06-12 21:39:28 +0200

    pNotify for the sounds
    pNotify for the sounds

---
+++ pNotify/__resource.lua
@@ -0,0 +1,19 @@
+description "Simple Notification Script using https://notifyjs.com/"
+
+ui_page "html/index.html"
+
+client_script "cl_notify.lua"
+
+export "SetQueueMax"
+export "SendNotification"
+
+files {
+    "html/index.html",
+    "html/pNotify.js",
+    "html/noty.js",
+    "html/noty.css",
+    "html/themes.css",
+    "html/sound-example.wav",
+	"html/Anchordown.ogg",
+    "html/Anchorup.ogg"
+}
